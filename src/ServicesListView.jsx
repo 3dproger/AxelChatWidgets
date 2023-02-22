7cd5e7eca4bafe47ca996f945642c1c3102e3043
Alexander <megag96@mail.ru>
2023-02-22 04:56:04 +0300

    Added status indicator

--- src/ServicesListView.jsx
+++ src/ServicesListView.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Avatar from '@mui/material/Avatar';
+import StyledBadge from "@mui/material/Badge"
 
 export class ServiceView extends React.Component {
     static propTypes = {
@@ -27,7 +29,14 @@ export class ServiceView extends React.Component {
 
         return (
             <span>
-                <img className="badge" alt="" src={"./images/" + service.type_id + "-icon.svg"} height="20"/>
+                <StyledBadge
+                    color={service.connection_state === "connected" ? "success" : "error"}
+                    overlap="circular"
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                    variant="dot"
+                    >
+                    <Avatar alt="" variant="square" src={"./images/" + service.type_id + "-icon.svg"}/>
+                </StyledBadge>
                 <span className="text">{service.viewers !== -1 ? service.viewers : ""}</span>
             </span>
         )
@@ -51,8 +60,7 @@ export class ServicesListView extends React.Component {
                 {this.props.services.map((service, idx) => (
                     <ServiceView key={idx} service={service} />
                 ))}
-
-                <img alt="" className="badge" src="./images/viewer.svg"/>
+                <Avatar alt="" variant="square" src="./images/viewer.svg"/>
                 <span className="text">{this.props.appState.viewers > -1 ? this.props.appState.viewers : "?"}</span>
             </div>
         )
