c3f00e01f61b0fa5ca0618f8c1566ce37c9d0d59
Alexander <megag96@mail.ru>
2023-06-19 04:46:28 +0300

    ui

--- src/MessagesWidgetEditorView.jsx
+++ src/MessagesWidgetEditorView.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { MessagesListView } from './MessagesListView';
-import { InputNumber, Select, Space, ColorPicker, Checkbox } from 'antd';
+import { InputNumber, Select, Space, Card, ColorPicker, Checkbox } from 'antd';
 
 class EditorPanel extends React.Component {
     static defaultProps = {
@@ -18,54 +18,63 @@ class EditorPanel extends React.Component {
 
     render() {
         return (
-            <Space wrap>
-                Shape:
-                <Select
-                    defaultValue={this.props.settings.item.avatar.shape}
-                    onChange={(value) => {
-                        this.props.settings.item.avatar.shape=value
-                        this.updateSettings()
-                    }}
-                    style={{
-                        width: 100,
-                    }}
-                    options={[
-                        {
-                            label: "Square",
-                            value: "square"
-                        },
-                        {
-                            label: "Circle",
-                            value: "circle"
-                        },
-                    ]}
+            <Space wrap direction="vertical">
+                <Card title="Avatar" size="small">
+                    Shape:
+                    <Select
+                        defaultValue={this.props.settings.item.avatar.shape}
+                        onChange={(value) => {
+                            this.props.settings.item.avatar.shape=value
+                            this.updateSettings()
+                        }}
+                        style={{
+                            width: 100,
+                        }}
+                        options={[
+                            {
+                                label: "Square",
+                                value: "square"
+                            },
+                            {
+                                label: "Circle",
+                                value: "circle"
+                            },
+                        ]}
                     />
 
-                Size:
-                <InputNumber
-                    min={1}
-                    max={500}
-                    defaultValue={this.props.settings.item.avatar.size}
-                    onChange={(value) => {
-                        this.props.settings.item.avatar.size=value
-                        this.updateSettings()
-                    }} />
+                    <br/>
+
+                    Size:
+                    <InputNumber
+                        min={1}
+                        max={500}
+                        defaultValue={this.props.settings.item.avatar.size}
+                        onChange={(value) => {
+                            this.props.settings.item.avatar.size=value
+                            this.updateSettings()
+                        }} />
                 
-                <Checkbox
-                    defaultChecked={this.props.settings.item.authorName.useOnlyDefaultColor}
-                    onChange={(e) =>{
-                        this.props.settings.item.authorName.useOnlyDefaultColor = e.target.checked
-                    }}>
-                    Use only default color
-                </Checkbox>
+                </Card>
                 
-                Default color:
-                <ColorPicker
-                    defaultValue={this.props.settings.item.authorName.defaultColor}
-                    onChange={(value) => {
-                        this.props.settings.item.authorName.defaultColor = value.toHexString()
-                        this.updateSettings()
-                    }} />
+                <Card title="Author name" size="small">
+                    <Checkbox
+                        defaultChecked={this.props.settings.item.authorName.useOnlyDefaultColor}
+                        onChange={(e) =>{
+                            this.props.settings.item.authorName.useOnlyDefaultColor = e.target.checked
+                        }}>
+                        Use only default color
+                    </Checkbox>
+
+                    <br/>
+                    
+                    Default color:
+                    <ColorPicker
+                        defaultValue={this.props.settings.item.authorName.defaultColor}
+                        onChange={(value) => {
+                            this.props.settings.item.authorName.defaultColor = value.toHexString()
+                            this.updateSettings()
+                        }} />
+                </Card>
             </Space>
         )
     }
@@ -86,18 +95,38 @@ export class MessagesWidgetEditorView extends React.Component {
 
     render() {
         return (
-            <div>
-                <table>
-                <tr>
-                    <td><MessagesListView messages={this.props.messages} settings={this.state.settings.item} /></td>
-                    <td><EditorPanel
+            <Space
+                direction="horizontal"
+                style={{
+                    backgroundColor: '#F0F2F5',
+                    height: '100vh',
+                    width: '100%',
+                }}>
+                <Card
+                    title="Chat"
+                    bordered={false}
+                    style={{
+                        height: 600,
+                        width: 300,
+                    }}
+                    >
+                    <MessagesListView messages={this.props.messages} settings={this.state.settings.item} />
+                </Card>
+
+                <Card
+                    title="Settings"
+                    bordered={false}
+                    style={{
+                        width: 300,
+                    }}
+                    >
+                    <EditorPanel
                         onChange={(settings)=>{
                             this.setState({ settings: settings })
                         }}
-                        /></td>
-                </tr>
-                </table>  
-            </div>
+                        />
+                </Card>
+            </Space>
         )
     }
 }
