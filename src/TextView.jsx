791798c0f57924f32631943cc42a562882b7be50
MnkyArts <liamrobinson5@web.de>
2023-11-03 03:04:18 +0100

    Added colon

--- src/TextView.jsx
+++ src/TextView.jsx
@@ -1,48 +1,57 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Typography } from 'antd';
+import React from "react";
+import PropTypes from "prop-types";
+import { Typography } from "antd";
 const { Text } = Typography;
 
 export class TextView extends React.Component {
-    static propTypes = {
-        text: PropTypes.string.isRequired,
-    }
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  };
 
-    static defaultProps = {
-        text: "",
-        settings: {
-            color: "#00ffff",
-            family: "Roboto",
-            size: 20,
-            weight: "400",
-            italic: false,
-            // text-decoration
-            outlineWidth: 1,
-            outlineColor: "#000000",
-        }
-    }
+  static defaultProps = {
+    text: "",
+    settings: {
+      color: "#00ffff",
+      family: "Roboto",
+      size: 20,
+      weight: "400",
+      italic: false,
+      // text-decoration
+      outlineWidth: 1,
+      outlineColor: "#000000",
+    },
+  };
 
-    static getAvailableFontsFamilies() {
-        return ['Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue']
-    }
+  static getAvailableFontsFamilies() {
+    return [
+      "Segoe UI",
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+    ];
+  }
 
-    render() {
-        const settings = this.props.settings
+  render() {
+    const settings = this.props.settings;
 
-        const style = {
-            color: settings.color,
-            fontSize: settings.size,
-            fontWeight: settings.weight,
-            fontStyle: settings.italic ? "italic" : "normal",
+    const style = {
+      color: settings.color,
+      fontSize: settings.size,
+      fontWeight: settings.weight,
+      fontStyle: settings.italic ? "italic" : "normal",
 
-            WebkitTextStrokeWidth: settings.outlineWidth,
-            WebkitTextStrokeColor: settings.outlineColor,
-        }
+      WebkitTextStrokeWidth: settings.outlineWidth,
+      WebkitTextStrokeColor: settings.outlineColor,
+    };
 
-        return (
-            <Text className='text' style={style}>
-                {this.props.text}
-            </Text>
-        )
-    }
+    return (
+      <Text className="text" style={style}>
+        {this.props.text}
+      </Text>
+    );
+  }
 }
