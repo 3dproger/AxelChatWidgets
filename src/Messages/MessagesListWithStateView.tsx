f0120acfea2e7f0fe39d2c7c4348f391afe39b73
Your Name <your.email@example.com>
2026-06-13 11:09:20 +0300

    minifix

--- src/Messages/MessagesListWithStateView.tsx
+++ src/Messages/MessagesListWithStateView.tsx
@@ -14,6 +14,7 @@ export function MessagesListWithStateView({messages}: MessagesListWithStateViewP
     return (
         <div>
             <MessagesListView messages={messages}/>
+            <br/>
             <PlatformStateListView/>
         </div>
     );
